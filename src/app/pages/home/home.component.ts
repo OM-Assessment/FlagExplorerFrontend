b39effa68e62294a37f6e7d9e0cda42e546053b6
TulaniMak <makhoncot@gmail.com>
2025-04-16 12:07:56 +0200

    initial commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import { Country } from '../../country.model';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ApiService } from '../../services/api.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true, 
+  imports: [
+    CommonModule,
+    MatProgressSpinnerModule
+    ],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  countries: Country[] = [];
+  loading = true;
+  error: string | null = null;
+  detailedCountries: { [name: string]: Country } = {};
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.loadCountries();
+  }
+
+  loadCountries(): void {
+    this.loading = true;
+    this.error = null;
+    
+    this.apiService.getCountries().subscribe({
+      next: (countries) => {
+        console.log('API Response:', countries); 
+        this.countries = countries;
+        this.loading = false;
+        
+      },
+      error: (err) => {
+        this.error = 'Failed to load countries';
+        this.loading = false;
+      }
+    });
+  }
+
+  loadCountryDetails(name: string): void {
+    if (!this.detailedCountries[name]) {
+      this.apiService.getCountryDetails(name).subscribe({
+        next: (details) => {
+          this.detailedCountries[name] = details;
+          // Update the country in the main array
+          const index = this.countries.findIndex(c => c.name === name);
+          if (index > -1) {
+            this.countries[index] = { ...this.countries[index], ...details };
+          }
+        },
+        error: (err) => {
+          console.error(`Failed to load details for ${name}`, err);
+          this.error = `Failed to load details for ${name}`;
+        }
+      });
+    }
+}
+
+}
